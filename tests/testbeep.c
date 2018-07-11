666c2ed7e458cad8c25eb33309dc10f00cba7edb
Ryan C. Gordon <icculus@icculus.org>
2018-07-11 01:13:42 -0400

    A DosBeep() test program.

---
+++ tests/testbeep.c
@@ -0,0 +1,22 @@
+#define INCL_DOS
+#include <os2.h>
+
+int main(void)
+{
+    DosBeep(250, 100);
+    DosBeep(300, 100);
+    DosBeep(350, 100);
+    DosBeep(400, 100);
+    DosBeep(450, 100);
+    DosBeep(475, 100);
+    DosBeep(490, 100);
+    DosBeep(497, 100);
+    DosBeep(500, 100);
+    return 0;
+} // main
+
+// end of testbeep.c ...
+
+
+
+
