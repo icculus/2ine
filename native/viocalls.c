6346c0a38b414bd85f960ce6bf45aa835a442e33
Andreas Peters <ap@aventer.biz>
2021-12-28 13:30:20 +0100

    make cmake more flexible for different linux distributions

--- native/viocalls.c
+++ native/viocalls.c
@@ -16,18 +16,7 @@
 #include <unistd.h>
 #include <ctype.h>
 
-// CMake searches for a whole bunch of different possible curses includes
-#if defined(HAVE_NCURSESW_NCURSES_H)
-#include <ncursesw/ncurses.h>
-#elif defined(HAVE_NCURSESW_CURSES_H)
-#include <ncursesw/curses.h>
-#elif defined(HAVE_NCURSESW_H)
-#include <ncursesw.h>
-#elif defined(HAVE_CURSES_H)
-#include <curses.h>
-#else
-#error ncurses gui enabled, but no known header file found
-#endif
+#include <ncurses.h>
 
 #include <locale.h>
 
